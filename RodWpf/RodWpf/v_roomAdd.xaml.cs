8e4e53e193faf070a3ad4f0fd4a171912f0538a6
Piotr Piorecki <piotrpiorecki@gmail.com>
2016-06-11 15:04:31 +0200

    First table and form: Rooms

---
+++ RodWpf/RodWpf/v_roomAdd.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RodWpf
+{
+    /// <summary>
+    /// Interaction logic for v_roomAdd.xaml
+    /// </summary>
+    public partial class v_roomAdd : Window
+    {
+        public v_roomAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RodWpf/UnitTestZaliczenie/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UnitTestZaliczenie
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
